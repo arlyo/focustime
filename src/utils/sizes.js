8252074262e1ba444ea92bb747fe30220bd7f28d
arlyo <arly.ordonez@gmail.com>
2024-04-24 13:45:49 -0600

    update fontSizes to fontSize

--- src/utils/sizes.js
+++ src/utils/sizes.js
@@ -1,12 +1,12 @@
 // src/utils/sizes.js
-export const fontSizes = {
+export const fontSize = {
   sm: 8,
   md: 16,
   lg: 24,
   xl: 32,
   xxl: 40,
-  xxxl: 80,
-};
+  xxxl: 80
+}
 
 export const spacing = {
   sm: 8,
@@ -14,5 +14,5 @@ export const spacing = {
   lg: 24,
   xl: 32,
   xxl: 40,
-  xxxl: 80,
-};
+  xxxl: 80
+}
